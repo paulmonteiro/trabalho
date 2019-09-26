7cf52087b38a5723bc2a0e055abd79740631db6b
Paulo Monteiro <paulo.isidro@yahoo.com>
2019-09-26 22:38:36 +0100

    primeiro commit, com o esboço do que vai ser feito

--- tarefa1/tarefa1.py
+++ tarefa1/tarefa1.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import random
+#cria uma lista de pontuacoes compreendidas entre 0 e 20, de forma aleatoria:
+lista_pontuacoes = [random.randint(0,20) for i in range(10)]
+
+print(lista_pontuacoes)
+
+
+#metodo para encontrar a pontuaçao mais elevada:
+#recebe uma lista de pontuações, faz uma copia dessa lista, percorre os elementos e devolve o maior
+
+
+
+#encontrar as tres pontuaçoes mais elevadas:
+
+#retorna a ultima pontuação a ser adicionada à lista:
