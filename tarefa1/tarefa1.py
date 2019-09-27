98e52dda47246d0b1668fc92b8d2749227d2a6cb
Paulo Monteiro <paulo.isidro@yahoo.com>
2019-09-27 01:14:44 +0100

    tarefa 1 : Criar funções ou métodos que, de uma lista de pontuações, devolvam a pontuação mais alta, as três pontuações mais altas e a ultima pontuação a ser adicionada à lista.

--- tarefa1/tarefa1.py
+++ tarefa1/tarefa1.py
@@ -1,15 +1,39 @@
 import random
-#cria uma lista de pontuacoes compreendidas entre 0 e 20, de forma aleatoria:
+# cria uma lista de 10 pontuacoes compreendidas entre 0 e 20, de forma aleatoria:
 lista_pontuacoes = [random.randint(0,20) for i in range(10)]
 
-print(lista_pontuacoes)
+print(f'Lista de pontuações: {lista_pontuacoes}')
 
+# metodo para encontrar a pontuaçao mais elevada:
+def encontra_maior(pontuacoes) :
+    # copia da lista original para se poder ordenar sem alterar a original
+    pontuacoes = pontuacoes[:]
+    # ordena a lista:
+    pontuacoes.sort(reverse=True)
+    # devolve a pontuação mais elevada:
+    return(pontuacoes[0])
 
-#metodo para encontrar a pontuaçao mais elevada:
-#recebe uma lista de pontuações, faz uma copia dessa lista, percorre os elementos e devolve o maior
+def remove_duplicados(lista_com_duplicados) :
+    # convertendo para set ficamos sem duplicados
+    # em seguida converte novamente para lista para se poder fazer slices
+    lista_sem_duplicados = list(set(lista_com_duplicados))
+    return lista_sem_duplicados
 
+# encontrar as tres pontuaçoes mais elevadas:
+def encontra_tres_maiores(pontuacoes) :
+    pontuacoes = remove_duplicados(pontuacoes)
+    # copia da lista original para se poder ordenar sem alterar a original
+    pontuacoes = pontuacoes[:]
+    pontuacoes.sort(reverse=True)
+    return pontuacoes[0:3]
 
+maior_pontuacao = encontra_maior(lista_pontuacoes)
+print(f'Maior pontuação: {maior_pontuacao}')
 
-#encontrar as tres pontuaçoes mais elevadas:
+tres_mais_elevadadas = encontra_tres_maiores(lista_pontuacoes)
+# converte a list para string:
+tres_mais_elevadadas = ', '.join(str(x) for x in tres_mais_elevadadas)
+print(f'As três pontuações mais elevadas são: {tres_mais_elevadadas}')
 
-#retorna a ultima pontuação a ser adicionada à lista:
+ultima_adicionada = lista_pontuacoes[-1]
+print(f'A ultima pontuação a ser adicionada a lista foi: {ultima_adicionada}')
