81f9bd9d1f9932054b7308e8c05af8e31c393544
Paulo Monteiro <paulo.isidro@yahoo.com>
2019-11-12 20:16:13 +0000

    Work in progress...

--- Task 5/task5.py
+++ Task 5/task5.py
@@ -1,27 +1,16 @@
 ordered_lists = [[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
-#ordered_lists = [[4], [0], [5]]
 
-print(f'Number of lists: {len(ordered_lists)}')
+shortest_list = min(map(len, ordered_lists))-1
 
-def determine_min_range():
-    current_min = 0
-    current_max = 0
-    current_range = []
-    determined_min = 0
-    determined_max = 0
-    determined_range = []
-    for i in range(0,len(ordered_lists)):
-        # determine the shortest list, this will be used to end the iteration through the elements
-        element_length = len(ordered_lists[i])
-        if i == 0:
-            min_length = element_length
-        else:
-            if element_length < min_length:
-                min_length = element_length
-        for j in range(0,min_length):
-            print(ordered_lists[i][j])
-
-    return determined_range
-
-range = determine_min_range()
-print(f'The minimum range common to all lists is: {range}')
+i = 0
+list_of_items = []
+while i <= shortest_list:
+    for element in ordered_lists:
+        list_of_items.append(element[0])
+    print(list_of_items, end='')
+    print(';')
+    # send list to find min, max and range
+    list_of_items = []
+    for element in ordered_lists:
+        del element[0]
+    i = i + 1
