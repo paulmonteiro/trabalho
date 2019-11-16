f5f4496e0eb7799ace87cedf659f8e9a8d0934d4
Paulo Monteiro <paulo.isidro@yahoo.com>
2019-11-16 20:07:19 +0000

    Work in progress...

--- Task 5/task5.py
+++ Task 5/task5.py
@@ -1,16 +1,38 @@
 ordered_lists = [[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
 
-shortest_list = min(map(len, ordered_lists))-1
-
+list_of_items=[]
+shortest_interval = []
 i = 0
-list_of_items = []
-while i <= shortest_list:
+while True:
     for element in ordered_lists:
         list_of_items.append(element[0])
-    print(list_of_items, end='')
-    print(';')
-    # send list to find min, max and range
-    list_of_items = []
+
+    # find min max range
+    min_value = min(list_of_items)
+    max_value = max(list_of_items)
+    range = max_value - min_value
+    if i == 0:
+        min_range = range
+        shortest_interval = [min_value, max_value]
+    else:
+        if range < min_range:
+            min_range = range
+            shortest_interval = [min_value,max_value]
+
+    print(f'Iteration {i}:')
+    print(f'Min: {min_value} Max: {max_value} Range: {range}')
+
+    # remove the minimum value from ordered_lists:
     for element in ordered_lists:
-        del element[0]
+        if element[0] == min_value:
+            del element[0]
+
+    print(ordered_lists)
+    list_of_items = []
+    for j,element in enumerate(ordered_lists):
+        if len(element) == 0:
+            print(f'\nNo more items in element {j}.')
+            print('\n')
+            print(f'The shortest interval is: {shortest_interval}')
+            exit(0)
     i = i + 1
